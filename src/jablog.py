d84b0983ccf0b873aeeac55b22f3fe31106a8a54
Diego Guerra <diego.guerra.suarez@gmail.com>
2008-02-28 12:42:23 +0000

    Small bugfix
    git-svn-id: https://jabbs.svn.sourceforge.net/svnroot/jabbs/trunk@40 1c16dc6d-1841-0410-8156-d696a23074c8

--- src/jablog.py
+++ src/jablog.py
@@ -8,14 +8,14 @@ class Jablog(jabbs.Controller):
         self.file = open("log.txt", 'w')
         jabbs.Controller.__init__(self, conversation, type)
     def controller(self):
-        return [("^end_logging", self.end),
+        return [("^end_logging", self.bye),
                 (".*", self.log)]
     
     def log(self, stanza):
-        self.file.write( stanza.get_from().as_string() + ">" + stanza.get_body() +"\n")
+        self.file.write( stanza.get_from().as_string() + "> " + stanza.get_body() +"\n")
         return self.no_message()
     
-    def end(self, stanza):
+    def bye(self, stanza):
         self.file.close()
         return self.end("ended logging")
         
